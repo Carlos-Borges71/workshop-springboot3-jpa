8a300fc32af7fe38008838d43a328d46cef9a112
Carlos Borges <kaca_master@hotail.com>
2024-04-20 20:15:43 -0300

    Payment, one-to-one association

--- src/main/java/com/carlosborges/config/TestConfig.java
+++ src/main/java/com/carlosborges/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.carlosborges.entities.Category;
 import com.carlosborges.entities.Order;
 import com.carlosborges.entities.OrderItem;
+import com.carlosborges.entities.Payment;
 import com.carlosborges.entities.Product;
 import com.carlosborges.entities.User;
 import com.carlosborges.entities.enums.OrderStatus;
@@ -83,5 +84,8 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }

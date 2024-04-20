dece2331a89acc3affef1eac7488a37e8faacccb
Carlos Borges <kaca_master@hotail.com>
2024-04-20 20:36:48 -0300

    SubTotal & Total methods

--- src/main/java/com/carlosborges/entities/Order.java
+++ src/main/java/com/carlosborges/entities/Order.java
@@ -38,7 +38,7 @@ public class Order implements Serializable{
     private User client;
     
     @OneToMany(mappedBy  = "id.order")
-    private Set<OrderItem> itens = new HashSet<>();
+    private Set<OrderItem> items = new HashSet<>();
     
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
@@ -89,7 +89,7 @@ public class Order implements Serializable{
     }
     
     public Set<OrderItem> getItems(){
-    	return itens;
+    	return items;
     }
     
 
@@ -99,6 +99,14 @@ public class Order implements Serializable{
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();			
+		}
+		return sum;
 	}
 
 	@Override
